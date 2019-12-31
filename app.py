a1ed9fdb8e927f7607485c171e50f8fd53d2b2ab
OwenCookman <ozzycookman@hotmail.com>
2019-12-31 00:48:27 +0000

    Changed host to 0.0.0.0

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ def search():
 
 
 if __name__ == "__main__":
-    app.run(host=os.environ.get("IP"),
+    app.run(host="0.0.0.0",
             port=int(os.environ.get("PORT")),
             debug=True)
