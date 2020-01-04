487698639f36eee39bf24791263fd59ded781eb0
OwenCookman <ozzycookman@hotmail.com>
2020-01-04 23:00:13 +0000

    Created Rout for results.html and function to print to console for searchbar

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
-import requests as req
+import requests
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+
+def search_result(search_text):
+    resp = requests.get(url="http://openlibrary.org/search.json?q=" +
+                        search_text + "&mode=ebooks&has_fulltext=true")
+    json = resp.json()
+    xx = json['docs']
+    for item in xx:
+        if 'author_name' in item:
+            print(item['title'], item['author_name'])
 
 
 @app.route("/")
@@ -20,12 +30,15 @@ def profile():
     return render_template("profile.html")
 
 
+@app.route("/results", methods=["POST"])
+def results():
+    if request.method == "POST":
+        search_text = request.form['search']
+        search_result(search_text)
+        return render_template("results.html")
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",
-            port=int(os.environ.get("PORT")),
+    app.run(host="127.0.0.1",
+            port=5000,
             debug=True)
-
-
-h = {'Authorization': 'YOUR_REST_KEY'}
-resp = req.get("https://api2.isbndb.com/book/9781934759486", headers=h)
-print(resp.json())
