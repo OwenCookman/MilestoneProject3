2ad81db0849e9993a37072ee6a16c8cf82443081
OwenCookman <ozzycookman@hotmail.com>
2020-01-30 19:43:13 +0000

    shortened confirm_password on to new line

--- forms.py
+++ forms.py
@@ -8,7 +8,8 @@ class RegistrationForm(FlaskForm):
                            validators=[DataRequired(), Length(min=2, max=15)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),
+                                                                     EqualTo('password')])
     submit = SubmitField('Sign Up')
 
 
